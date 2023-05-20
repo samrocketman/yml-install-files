1c8709448fb6f5556df7fd17be2cbc32dbb8f908
Sam Gleske <sam.mxracer@gmail.com>
2023-05-20 17:55:47 -0400

    update release script

--- .ci/release.sh
+++ .ci/release.sh
@@ -18,7 +18,10 @@ tar -c \
   | gzip -9 > universal.tgz
 
 notes() {
-  awk '$0 ~ /^#/ && notes == 1 { exit; }; $0 ~ /^#/ {notes=1;}; notes == 1 {print}' < CHANGELOG.md
+  awk '
+    $0 ~ /^# download-utilities/ && notes == 1 { exit; };
+    $0 ~ /^# download-utilities/ {notes=1;};
+    notes == 1 {print}' < CHANGELOG.md
 }
 
 if [ "$#" -lt 1 ]; then
@@ -26,10 +29,21 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 
-if ! notes | head -n1 | grep "v$1\$"; then
+if ! notes | head -n1 | grep "v$1\$" > /dev/null; then
   echo 'ERROR: Release notes not updated for: '"v$1"
   exit 1
 fi
 
-mkdir scratch
-./download-utilities.sh download-utilities.yml gh
+if [ ! -x ./scratch/gh ]; then
+  mkdir scratch
+  ./download-utilities.sh download-utilities.yml gh
+fi
+
+git tag "v$1"
+git push origin refs/tags/"v$1":refs/tags/"v$1"
+
+./scratch/gh release create "v$1" --verify-tag \
+  --title "download-utilities v$1" \
+  --notes "$(notes)"
+
+./scratch/gh release upload "v$1" universal.tgz
