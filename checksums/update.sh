4d6c81dba670a71230c027cafd9c37983f169859
Sam Gleske <sam.mxracer@gmail.com>
2023-05-23 18:07:41 -0400

    Update checksum script optionally run in docker
    Only if not Linux.

--- checksums/update.sh
+++ checksums/update.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 
+force_yq=1
+skip_checksum=1
+export force_yq skip_checksum
+
 update_versions() (
   echo 'Checking for version updates.' >&2
-  docker run \
-    -e force_yq=1 \
-    -e os="$1" \
-    -e arch="$2" \
-    -e skip_checksum=1 \
-    -u "$(id -u):$(id -g)" \
-    -w "$PWD" \
-    -v "$PWD:$PWD" update /bin/bash -c \
-    './download-utilities.sh --update'
+  ./download-utilities.sh --update
 )
 
 update_arch() (
+  os="$1"
+  arch="$2"
+  export arch os
   echo "Update checksums for $1 $2." >&2
   # create checksum files
-  docker run \
-    -e force_yq=1 \
-    -e os="$1" \
-    -e arch="$2" \
-    -e skip_checksum=1 \
-    -u "$(id -u):$(id -g)" \
-    -w "$PWD" \
-    -v "$PWD:$PWD" update /bin/bash -c \
-    './download-utilities.sh; ./download-utilities.sh --checksum > "checksums/${os}-${arch}.sha256"'
+  ./download-utilities.sh
+  ./download-utilities.sh --checksum > "checksums/${os}-${arch}.sha256"
 )
 
-set -e
+set -euo pipefail
 
 [ -x ./download-utilities.sh ] || {
   echo 'Must be run from repository root.' >&2
   exit 1
 }
-docker build -t update -f checksums/Dockerfile checksums
+
+
+if [ ! "$(uname)" = Linux ]; then
+  docker build -t update -f checksums/Dockerfile checksums
+  docker run -it --rm
+    -u "$(id -u):$(id -g)" \
+    -w "$PWD" \
+    -v "$PWD:$PWD" update /bin/bash -c \
+      ./checksums/update.sh
+  exit $?
+fi
+
 update_versions
+
 if git diff --quiet --exit-code download-utilities.yml && \
   [ -z "${checksum:-}" ]; then
   echo 'No updates.' >&2
   exit
 fi
+
 update_arch Linux x86_64
 update_arch Linux aarch64
 update_arch Darwin x86_64
