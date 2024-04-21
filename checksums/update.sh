ad51c84bc33e444e2157b382f37a5d87c409cccb
Sam Gleske <sam.mxracer@gmail.com>
2024-04-21 15:44:51 -0400

    Update checksums/update.sh script

--- checksums/update.sh
+++ checksums/update.sh
@@ -1,43 +1,6 @@
 #!/bin/bash
 
-force_yq=1
-skip_checksum=1
-export force_yq skip_checksum
-
-update_versions() (
-  echo 'Checking for version updates.' >&2
-  ./download-utilities.sh --update
-)
-
-update_arch() (
-  os="$1"
-  arch="$2"
-  export arch os
-  echo "Update checksums for $1 $2." >&2
-  # create checksum files
-  ./download-utilities.sh
-  ./download-utilities.sh --checksum > "checksums/${os}-${arch}.sha256"
-)
-
-set -euo pipefail
-
-[ -x ./download-utilities.sh ] || {
-  echo 'Must be run from repository root.' >&2
-  exit 1
-}
-
-
-if [ "${1:-}" = docker ]; then
-  docker build -t update -f checksums/Dockerfile checksums
-  docker run -it --rm
-    -u "$(id -u):$(id -g)" \
-    -w "$PWD" \
-    -v "$PWD:$PWD" update /bin/bash -c \
-      ./checksums/update.sh
-  exit $?
-fi
-
-update_versions
+./download-utilities.sh --update
 
 if git diff --quiet --exit-code download-utilities.yml && \
   [ -z "${checksum:-}" ]; then
@@ -45,7 +8,9 @@ if git diff --quiet --exit-code download-utilities.yml && \
   exit
 fi
 
-update_arch Darwin arm64
-update_arch Darwin x86_64
-update_arch Linux aarch64
-update_arch Linux x86_64
+# Update primary utilities
+./download-utilities.sh --checksum \
+  -I Darwin:arm64 \
+  -I Darwin:x86_64 \
+  -I Linux:aarch64 \
+  -I Linux:x86_64
